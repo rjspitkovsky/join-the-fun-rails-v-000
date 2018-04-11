f21cda906cc659e34839d1aba6f397427fa332d2
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-11 21:01:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411205059) do
+ActiveRecord::Schema.define(version: 20180411210130) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 20180411205059) do
   add_index "passengers_taxis", ["taxi_id"], name: "index_passengers_taxis_on_taxi_id"
 
   create_table "rides", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "passenger_id"
+    t.integer  "taxi_id"
   end
 
   create_table "taxis", force: :cascade do |t|
