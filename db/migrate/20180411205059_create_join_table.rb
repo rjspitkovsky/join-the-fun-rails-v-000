6fa4b510381d0db0a14d1249aa7e1c92298ea417
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-11 20:53:05 +0000

    automatically backed up by learn

--- db/migrate/20180411205059_create_join_table.rb
+++ db/migrate/20180411205059_create_join_table.rb
@@ -1,8 +1,8 @@
 class CreateJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :, : do |t|
-      # t.index [:_id, :_id]
-      # t.index [:_id, :_id]
+    create_join_table :taxis, :passengers do |t|
+       t.index :taxi_id 
+       t.index :passenger_id 
     end
   end
 end
